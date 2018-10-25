d5b861fcac121a3c8fcc07759559ffec13fe293e
Cecephaer12 <35304896+Cecephaer12@users.noreply.github.com>
2018-10-25 09:08:55 +0000

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,29 @@
+<?php
+$android_id = "54a234e2c740d3d2";
+$deviceCode = "353634091224708";
+$device_ip  = "114.5.146.115";
+$token = "794cNBpvYxi2XHJbULDD4bXlaNf1hGcemvwyFxNEZzLBNGlIuT5bG4OEiUWB4HecOcqqby25nPVkBFM";
+$tk = "ACFw0qFS1ChCkLPtwUbqabgSLInbm0TbWw9xdHRodw";
+$uuid = "2402f891243a40898cd95decab977ce9";
+$userid = "8073007";
+$sign = "46b1fba92a3dae3618962459e2d2141b";
+
+####################################################
+#|note:
+#| *untuk aktifitas nuyul multi akun
+#|  gunakan andoid_id yang berbeda setiap akun
+#|  gunakan devicecode (imei) yang berbeda setiap akun
+#|  gunakan device_ip yang berbeda setiap akun
+#| *untuk settingan lainya wajib sama 
+#|  dengan data dari packet capture
+#|
+####################################################
+#| cara mengaktifkan multi akun
+#| duplikat data di atas dari anroid_id => sign
+#| buat file baru dengan nama bebas terserah kamu
+#| aktifkan skrip di bawah dngn menghapus tanda pagar
+#| jika skrip di aktifkan maka hapus data di atas 
+#|#####################################################
+#| hapus pagar di bawah untuk mengaktifkan skrip
+
+# include(readline("masukan config: "));
